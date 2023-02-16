e185cb944cb1eafa348ea75085900234a17b668d
dylanmlilo <dylanmlilo12@gmail.com>
2023-02-16 15:36:00 -0800

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity - C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+*main - C program that prints without using prinf or puts
+*Return: Always 1
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
+
