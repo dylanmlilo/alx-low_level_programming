172af38c79a826ad20e5dbbc31eaedea6bcf3f92
dylanmlilo <dylanmlilo12@gmail.com>
2023-02-17 18:53:25 -0800

    Prints all the numbers of base 16 in lowercase, followed by a new line, can only use putchar

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 /**
- * main - prints all the numbers of base 10 from 0 to 9
+ * main - prints all the numbers of base 16 followed by a new linw
  * main - using putchar
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
+	char alpha;
 
 	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
+	}
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+	{
+		putchar(alpha);
 	}
 	putchar('\n');
 	return (0);
