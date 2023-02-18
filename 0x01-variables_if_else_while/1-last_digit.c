402ee0ec50222f1a674566158828064b5f729985
dylanmlilo <dylanmlilo12@gmail.com>
2023-02-17 19:41:28 -0800

    program will assign a random number to the variable n each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable n

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, num;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	num = (n % 10);
+	if (num > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, num);
+	}
+	else if (num == 0)
+	{
+		printf("Last digit of %d is %d and is zero\n", n, num);
+	}
+	else if (num < 6)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not zero\n", n, num);
+	}
+	return (0);
+}
