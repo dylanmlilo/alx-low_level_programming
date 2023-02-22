0639494b7afcb56f11ce5cb648911060af84fe00
dylanmlilo <dylanmlilo12@gmail.com>
2023-02-21 17:05:46 -0800

    Function that prints 10 times the alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+
+/**
+ * main - function that prints 10 times the alphabet, in lowercase
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet_x10();
+    return (0);
+}
