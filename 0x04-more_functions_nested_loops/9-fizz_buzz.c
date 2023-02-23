aa428b0614ea73ea0333fac23b1267a01bec6447
dylanmlilo <dylanmlilo12@gmail.com>
2023-02-23 14:34:39 -0800

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,28 +15,32 @@
  */
 int main(void)
 {
-	int i = 1;
+	int i;
 
-	while (i <= 100)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 5 == 0 && i % 3 == 0)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("FizzBuzz ");
+			printf(" Fizz");
 		}
-		else if (i % 5 == 0)
+		else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
-		else if (i % 3 == 0)
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("Fizz ");
+			printf(" FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
 		}
 		else
 		{
-			printf("%d ", i);
+			printf(" %d", i);
 		}
-		i++;
 	}
 	printf("\n");
 	return (0);
 }
+
