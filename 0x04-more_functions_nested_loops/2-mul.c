ae84c58642c69955db9124a6c7a2194fd4e2e315
dylanmlilo <dylanmlilo12@gmail.com>
2023-02-23 08:18:41 -0800

    2. Collaboration is multiplication - function that multiplies two integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,13 +2,14 @@
 /**
  * mul -multiplies int a and in b
  * @a: integer to be multiplied with b
- * @a: integer to be multiplied with a
+ * @b: integer to be multiplied with a
  *
  * Return: answer
  */
 int mul(int a, int b)
 {
 	int ans;
+
 	ans = a * b;
 	return (ans);
 }
