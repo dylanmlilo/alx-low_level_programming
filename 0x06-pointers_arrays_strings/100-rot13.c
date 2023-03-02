b00f32672712b8af4e365059e6109f124ee89eba
dylanmlilo <dylanmlilo12@gmail.com>
2023-03-02 14:02:46 -0800

    8. rot13 - function that encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - function that encodes a string using rot13
+ *
+ * You can only use if statement once in your code
+ * You can only use two loops in your code
+ * You are not allowed to use switch
+ * You are not allowed to use any ternary operation
+ *
+ * @sr: string array to be encoded
+ *
+ * Return: (sr)
+ */
+
+char *rot13(char *sr)
+{
+	int i, j;
+
+	char s1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; sr[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (sr[i] == s1[j])
+			{
+				sr[i] = s2[j];
+			}
+		}
+	}
+	return (sr);
+}
+
