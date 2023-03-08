b6b22c0950c2b14840f8b6027faacaaf249a4f14
dylanmlilo <dylanmlilo12@gmail.com>
2023-03-08 14:42:49 -0800

    Function that returns the value of x raised to the power of y

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,7 +6,7 @@ int actual_prime(int n, int i);
 /**
  * is_prime_number - function that returns 1 if the input integer
  * is a prime number, otherwise return 0
- * 
+ *
  * @n: number to check
  *
  * Return: 1 if n is prime, 0 if it is not
@@ -25,7 +25,7 @@ int is_prime_number(int n)
 }
 
 /**
- * recursine_prime - calculates if a number is prime recursively
+ * recursive_prime - calculates if a number is prime recursively
  *
  * @n: number to check
  * @i: iterator
@@ -33,13 +33,18 @@ int is_prime_number(int n)
  * Return: 1 if n is prime, 0 if not
  */
 
-int actual_prime(int n, int i)
+int recursive_prime(int n, int i)
 {
 	if (i == 1)
 	{
 		return (1);
 	}
-	if (n % i == 0 && i > 0)
+	else if (n % i == 0 && i > 0)
 	{
-		return (0);}	return (actual_prime(n, i - 1));
+		return (0);
+	}
+	else
+	{
+		return (recursive_prime(n, i - 1));
+	}
 }
