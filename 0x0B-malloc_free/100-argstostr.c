99139104f58f89d6ce5e7417c4a797f50e2c47e2
dylanmlilo <dylanmlilo12@gmail.com>
2023-03-15 14:32:21 -0700

    Function that concatenates all the arguments of your program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,41 +3,48 @@
 #include <stdlib.h>
 
 /**
- * argstostr - main entry		
- * @ac: int input
+ * argstostr - function that concatenates all the arguments of your program
+ *
+ * @ac: integer
  * @av: double pointer arrray
- * Return: 0
+ *
+ * Return: 0 (success)
  */
 
-char *argstostr(int ac, char **av)		
+char *argstostr(int ac, char **av)
 {
-	int i, n, r = 0, l = 0;		
+	int i, j, k, l;
 	char *str;
-		
+
+	k = l = 0;
+
 	if (ac == 0 || av == NULL)
+	{
 		return (NULL);
-		
+	}
 	for (i = 0; i < ac; i++)
 	{
-		for (n = 0; av[i][n]; n++)	
+		for (j = 0; av[i][j]; j++)
 			l++;
 	}
-	l += ac;	
+	l += ac;
 	str = malloc(sizeof(char) * l + 1);
 	if (str == NULL)
+	{
 		return (NULL);
-	for (i = 0; i < ac; i++)	
+	}
+	for (i = 0; i < ac; i++)
 	{
-	for (n = 0; av[i][n]; n++)
-	{	
-		str[r] = av[i][n];
-		r++;	
-	}
-	if (str[r] == '\0')
+	for (j = 0; av[i][j]; j++)
 	{
-		str[r++] = '\n';
+		str[k] = av[i][j];
+		k++;
+	}
+	if (str[k] == '\0')
+	{
+		str[k++] = '\n';
 	}
 	}
-	return (str);		
+	return (str);
 }
 
