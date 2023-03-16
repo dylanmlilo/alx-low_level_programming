21aab910dc63a51cd013fcb6783a95b98d3824fc
dylanmlilo <dylanmlilo12@gmail.com>
2023-03-16 16:01:04 -0700

    Munction that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**		
+ * *malloc_checked - function that allocates memory using malloc
+ *
+ * @b: number of bytes to be  allocated
+ *
+ * Return: a pointer to the allocated memory
+ */
+		
+void *malloc_checked(unsigned int b)
+{
+
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}	
+	return (p);		
+}
