f2e0bf31465bdcf2c1fdfd50a12f39d46d02d3d3
dylanmlilo <dylanmlilo12@gmail.com>
2023-04-04 15:51:49 -0700

    function that finds the loop in a linked list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -30,5 +30,6 @@ typedef struct listint_s
 	listint_t *reverse_listint(listint_t **head);
 	size_t print_listint_safe(const listint_t *head);
 	size_t free_listint_safe(listint_t **h);
+	listint_t *find_listint_loop(listint_t *head);
 
 #endif
